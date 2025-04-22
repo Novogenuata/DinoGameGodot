d188c182d8ffef7ac3b8739818a1baec981531dc
s <Samantha.Jones@stu.crc-lennox.qc.ca>
2025-04-22 13:27:46 -0400

    simple logic for menu

--- MainScenes/main_menu.gd
+++ MainScenes/main_menu.gd
@@ -2,10 +2,18 @@ extends Control
 
 
 func _on_play_pressed() -> void:
+	#changes to game
 	SceneManager.change_scene("res://MainScenes/MainGame.tscn")
 	
 
 
 func _on_exit_pressed() -> void:
+	#closes
 	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	#change to settings menu
+	
 	pass # Replace with function body.
