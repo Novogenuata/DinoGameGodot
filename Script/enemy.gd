0fedfcf10d9571183b35d5490d912d1f933bfb36
Micah <micah.bayley@stu.crc-lennox.qc.ca>
2025-04-28 15:48:45 -0400

    Adjusted enemy stats, added random spawnpoints for enemies, enemies move toward the player.

--- Script/enemy.gd
+++ Script/enemy.gd
@@ -3,12 +3,18 @@ extends Node2D
 @export var health: int
 @export var movement_speed: int
 
-@onready var player = $MainCharacter
+@onready var player = get_tree().get_nodes_in_group("player")[0]
 
 func _ready() -> void:
 	pass
 	
-func _physics_process(delta):
+func _process(delta):
 	if player:
-		rotation = (player.global_position - global_position).normalized()
-		position += transform.x * movement_speed * delta
+		# Direction to player
+		var direction = (player.global_position - global_position).normalized()
+		
+		# Move directly toward player
+		position += direction * movement_speed * delta
+		
+		# Rotate to face the movement direction
+		rotation = direction.angle()
--- Script/level_logic.gd
+++ Script/level_logic.gd
@@ -6,22 +6,21 @@ extends Node
 @onready var player = $"../MainCharacter"
 @onready var enemy_interval = $EnemyInterval
 @onready var enemies_left
+@onready var spawnpoints = $spawnpoints
 
 const slow_slime = preload("res://CharacterScenes/slow_slime.tscn")
 const fast_slime = preload("res://CharacterScenes/fast_slime.tscn")
 const tank_slime = preload("res://CharacterScenes/tank_slime.tscn")
 
 func _ready():
-	timer.start()
+	cooldown()
 
 func _process(delta: float):
-	if timer <= 0:
-		timer.stop()
-		new_level()
+	self.global_position = player.global_position
 
 func new_level():
-	enemy_interval = 1.2
-	enemies_left = 15 * level
+	enemy_interval.wait_time = 1.2
+	enemies_left = 2 * level
 	spawn_enemies()
 	
 func spawn_enemies():
@@ -29,17 +28,25 @@ func spawn_enemies():
 		var enemy_rng = randi_range(1, level)
 		match enemy_rng:
 			1:
-				spawn_slime("slow_slime")
+				spawn_slime(slow_slime)
 			2:
-				spawn_slime("fast_slime")
+				spawn_slime(fast_slime)
 			3:
-				spawn_slime("tank_slime")
+				spawn_slime(tank_slime)
+		enemies_left -= 1
+		print(enemies_left)
 		enemy_interval.start()
-		await enemy_interval
+		await enemy_interval.timeout
 	level += 1
-	timer.start()
+	cooldown()
 
 func spawn_slime(slime):
 	var slime_instance = slime.instantiate()
 	get_tree().root.add_child(slime_instance)
-	slime_instance.global_position = player.global_position
+	var random_spawn = spawnpoints.get_children()[randi_range(0, 3)]
+	slime_instance.global_position = random_spawn.global_position
+	
+func cooldown():
+	timer.start()
+	await timer.timeout
+	new_level()
