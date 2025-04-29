0804d753b1a6ec302ddb6b70997294c6c77d23e1
s <Samantha.Jones@stu.crc-lennox.qc.ca>
2025-04-29 08:45:15 -0400

    this is a test

--- Script/main_character.gd
+++ Script/main_character.gd
@@ -65,3 +65,4 @@ func _on_level_1_gate_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		#change to the water scene with the water gun
 		SceneManager.change_scene("res://MainScenes/water_scene.tscn")
+		#this is a test
