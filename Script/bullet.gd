9131ae49cb6ce110bde13445ff6101331487b949
Sigourney <2250485@crosemont.quebec.ca>
2025-05-02 09:02:00 -0400

    added enemy logic

--- Script/bullet.gd
+++ Script/bullet.gd
@@ -1,8 +1,16 @@
-extends Node2D
+extends Area2D
 
-
+@export var damage: int = 1
 const speed: int = 1000
+
 func _ready() -> void:
-	pass
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
 func _process(delta: float) -> void:
 	position += transform.x * speed * delta
+
+func _on_body_entered(body):
+	if body.is_in_group("enemy"):
+		if body.has_method("take_damage"):
+			body.take_damage(damage)
+		queue_free()
