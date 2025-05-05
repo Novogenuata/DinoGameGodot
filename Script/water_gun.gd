dbdc994e04bf19639da076ab5787b2c2e7f8c042
s <Samantha.Jones@stu.crc-lennox.qc.ca>
2025-05-05 10:37:37 -0400

    added logic to interact with water gun

--- Script/water_gun.gd
+++ Script/water_gun.gd
@@ -4,7 +4,6 @@ extends Node2D
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("body entered:", body.name)
 	if body.is_in_group("player"):
-		CoinManager.add_coin()
 		$Area2D/AudioStreamPlayer2D.play()
 		queue_free()
 	pass # Replace with function body.
