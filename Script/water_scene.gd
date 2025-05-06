f289199a52f29c0f441385a03dd10fae6fc94ff1
s <Samantha.Jones@stu.crc-lennox.qc.ca>
2025-05-06 09:22:48 -0400

    trying to add clamps

--- Script/water_scene.gd
+++ Script/water_scene.gd
@@ -5,3 +5,4 @@ func _on_exitgate_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		#change to the water scene with the water gun
 		SceneManager.change_scene("res://MainScenes/MainGame.tscn")
+		
