797709e156fa35f20a403b392e096781ebb635c3
s <Samantha.Jones@stu.crc-lennox.qc.ca>
2025-05-06 09:07:35 -0400

    changed the coin SFX, they now work

--- collectableScenes/coin.gd
+++ collectableScenes/coin.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("body entered:", body.name)
 	if body.is_in_group("player"):
 		CoinManager.add_coin()
-		$Area2D/AudioStreamPlayer2D.play()
-		queue_free()
-	pass # Replace with function body.
+
+		# Hide the coin visually and disable collision
+		visible = false
+		$Area2D.monitoring = false
+		$Area2D.set_deferred("monitorable", false)
+		
+		# Play pickup sound
+		var audio = $Area2D/AudioStreamPlayer2D
+		audio.play()
+
+		# Connect to sound's finished signal
+		if not audio.is_connected("finished", Callable(self, "_on_sound_finished")):
+			audio.connect("finished", Callable(self, "_on_sound_finished"))
+
+func _on_sound_finished():
+	queue_free()
