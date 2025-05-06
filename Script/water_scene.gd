c6bf5e1128d3b8f10d99cb9342ac24e02865020b
s <Samantha.Jones@stu.crc-lennox.qc.ca>
2025-05-06 09:52:04 -0400

    water_scene now infinite scroll

--- Script/water_scene.gd
+++ Script/water_scene.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 
+
+
 func _on_exitgate_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		#change to the water scene with the water gun
 		SceneManager.change_scene("res://MainScenes/MainGame.tscn")
-		
